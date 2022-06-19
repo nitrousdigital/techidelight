cf4b498c05b9a0bba4cbc612fbb22a13d7b84ad4
nitrousdigital <nitrousdigital@gmail.com>
2022-06-19 17:52:07 -0400

    sliding window approach to longest substring

--- src/com/nitrous/leetcode/longestsubstring/Solution.java
+++ src/com/nitrous/leetcode/longestsubstring/Solution.java
@@ -3,19 +3,28 @@ package com.nitrous.leetcode.longestsubstring;
 import java.util.HashSet;
 
 public class Solution {
+    /**
+     * A sliding window approach
+     * @param s
+     * @return
+     */
     public int lengthOfLongestSubstring(String s) {        
         int longest = 0;
-        for (int wordStart = 0 ; wordStart < s.length(); wordStart++) {
-            HashSet<Character> seen = new HashSet<Character>();
-            for (int wordEnd = wordStart; wordEnd < s.length(); wordEnd++) {
-                char c = s.charAt(wordEnd);
-                if (!seen.add(c)) {
-                    // duplicate character found
-                    break;
-                }
+        int wordStart = 0;
+        int wordEnd = 0;
+        HashSet<Character> seen = new HashSet<Character>();
+        while (wordEnd < s.length()) {
+            char c = s.charAt(wordEnd);
+            while (seen.contains(c) && wordStart < wordEnd) {
+                seen.remove(s.charAt(wordStart));
+                wordStart++;
             }
+            seen.add(c);
             longest = Math.max(seen.size(), longest);
+            
+            wordEnd++;
         }
+
         return longest;
     }
 }
