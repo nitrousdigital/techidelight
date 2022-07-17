5df6d97362f1419ffbe2b799587b2bf118b620aa
nitrousdigital <nitrousdigital@gmail.com>
2022-07-16 21:45:34 -0400

    hamming weight

---
+++ src/com/nitrous/leetcode/hammingweight/Solution.java
@@ -0,0 +1,28 @@
+package com.nitrous.leetcode.hammingweight;
+
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int count = 0;
+        while (n != 0) {
+            int x = n & 1;
+            if (x == 1) {
+                count++;
+            }
+            n = n >>> 1;
+        }
+        return count;
+    }
+    
+    public static void main(String[] args) {
+        int input = Long.valueOf("11111111111111111111111111111101", 2).intValue();
+        int actual = new Solution().hammingWeight(input);
+        int expected = 31;
+        if (actual == expected) {
+            System.out.println("Correct!");
+        } else {
+            System.err.println("Incorrect! Expected "+expected+" But Got "+actual+" for " + input);
+        }
+        
+    }
+}
